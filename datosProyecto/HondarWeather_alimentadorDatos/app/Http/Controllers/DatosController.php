5a5b7e3a1e00b17434a30ea978c37dc6e0d6a4a1
unknown <2daw3@HZ114480.H012108.NET>
2024-01-22 14:03:00 +0100

    Primer commit

---
+++ datosProyecto/HondarWeather_alimentadorDatos/app/Http/Controllers/DatosController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class DatosController extends Controller
+{
+    // Pedir datos a la api
+
+    // Gestionar datos
+
+    // Mandar datos a la bbdd
+}
