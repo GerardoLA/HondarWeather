f8ec4532c2ecbc659cdd71a4971c946827751045
Your Name <you@example.com>
2024-01-26 13:52:24 +0100

    Login Logout funcionando, mrc lugar historico

--- datosProyecto/HondarWeather/routes/api.php
+++ datosProyecto/HondarWeather/routes/api.php
@@ -4,7 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\API\RegisterController;
-use App\Http\Controllers\API\ProductController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -20,6 +20,7 @@ use App\Http\Controllers\API\ProductController;
 Route::post('register', [RegisterController::class, 'register']);
 Route::post('login', [RegisterController::class, 'login']);
 
-Route::middleware('auth:api')->group( function () {
-    Route::resource('products', ProductController::class);
+Route::middleware('auth:api')->group(function () {  
+    Route::get('logout', [RegisterController::class, 'logout']);
+
 });
