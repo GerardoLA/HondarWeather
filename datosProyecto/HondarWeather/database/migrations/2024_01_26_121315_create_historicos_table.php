84040277e37b87ff065b99b103ec0ea7ac832933
Your Name <you@example.com>
2024-02-05 10:24:39 +0100

    lunes running

---
+++ datosProyecto/HondarWeather/database/migrations/2024_01_26_121315_create_historicos_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('historico_datos', function (Blueprint $table) {
+            $table->date('fecha');
+            $table->float('temperatura');
+            $table->float('humedad');
+            $table->string('nombre');
+
+            //clave primaria
+            $table->primary(['nombre','fecha']);
+            //foreign key
+            $table->foreign('nombre')->references('nombre')->on('lugares');
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('historico_datos', function (Blueprint $table) {
+            $table->dropForeign(['nombre']);
+        });
+    
+    
+        Schema::dropIfExists('historico_datos');
+    }
+};
